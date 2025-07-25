08cce6885d1b5395ed110eafd98261c0c5628b32
eeeeexxxxxx <examexpresssss@gmail.com>
2025-07-25 21:01:25 +0300

    Update lib.rs

--- arbitrage_program/programs/arbitrage_program/src/lib.rs
+++ arbitrage_program/programs/arbitrage_program/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::solana_program::program::{invoke, invoke_signed};
 use anchor_lang::solana_program::instruction::{AccountMeta, Instruction};
 use anchor_lang::solana_program::compute_budget::ComputeBudgetInstruction;
 
-// TODO: заменить на реальный id после деплоя
+// свой ID
 declare_id!("33BQPv9UU9moaQPah9umUad4ovijbF2TVmxcGdSmeSJR");
 
 // MarginFi program id (mainnet)
